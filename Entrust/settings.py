7f42547903bd3e47098e402b4096e82619317139
skpatel <153447817+Sandip-364710@users.noreply.github.com>
2024-07-24 16:06:04 +0530

    Update settings.py

--- Entrust/settings.py
+++ Entrust/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 import os 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"employe", "static")]
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
